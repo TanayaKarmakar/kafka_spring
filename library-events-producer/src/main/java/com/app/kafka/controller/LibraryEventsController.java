88f5b21819a15c361277e376c8bcc88994fb2ceb
TanayaKarmakar <karmakar.tanaya@gmail.com>
2021-01-23 19:39:26 +0530

    kafka spring integration--wip

--- library-events-producer/src/main/java/com/app/kafka/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/app/kafka/controller/LibraryEventsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +29,16 @@ public class LibraryEventsController {
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
 	}
+
+	@PutMapping("/v1/libraryevent")
+	public ResponseEntity<?> updateLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws Exception {
+		if(libraryEvent.getLibraryEventId() == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the library event ID");
+		libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+		libraryProducer.sendLibraryEventApproach2(libraryEvent);
+		logger.info("after SendLibraryEvent");
+
+		return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
+	}
+
 }
